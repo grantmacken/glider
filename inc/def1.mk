27525bfaf1bbdfabf98ad038643688417ac8fda7
grantmacken <grantmacken@gmail.com>
2022-04-21 07:14:24 +1200

    add define files

---
+++ inc/def1.mk
@@ -0,0 +1,180 @@
+define restXQ_tpl
+module namespace _ = 'http://$(DEV_DOMAIN)/#routes';
+declare namespace cm ="http://commonmark.org/xml/1.0";
+import module namespace cmark = "http://xq/#cm_dispatch";
+
+declare
+  %rest:path("/$(DEV_DOMAIN)/{$$ITEM}")
+  %rest:GET
+  %rest:produces("text/html")
+  %output:method("html")
+function _:erewhon($$ITEM){
+  try {
+  let $$uri := 'http://$(DEV_DOMAIN)/' || $$ITEM
+  let $$resMap := 
+      map{'uri': $$uri, 
+          'domain': '$(DEV_DOMAIN)',
+          'collection' : _:dbCollection($$uri), 
+          'item':  _:dbItem($$uri)}
+  return
+    if ( fn:doc-available( $$uri  )) 
+    then
+      let $$key := 'document'
+      let $$value := $$uri => doc()
+      return
+        map:put($$resMap ,$$key, $$value) =>
+        _:getContent() =>
+        _:getFrontMatter() => 
+        _:pageLayout()
+    else _:notFound($$resMap) 
+  } catch * {(
+    _:svrErr(
+		map { 'uri': 'http://' || '$(DEV_DOMAIN):$($(DEV_PORT))/' || $$ITEM, 
+             'problem' : 'xQuery dynamic error',
+             'code' : $$err:code, 
+             'description' : $$err:description
+             } 
+      )
+   )}
+};
+
+declare function _:resHeader( $$map as map(*)) as element() {
+element rest:response {
+  element http:response {
+  attribute status { $$map?status },
+  attribute message {$$map?message },
+  element http:header {
+    attribute name {'Content-Type'},
+    attribute value {'text/html'}
+    }
+  }
+}
+};
+
+declare function _:getContent( $$map as map(*) ) as map(*){
+ let $$xDoc :=  $$map?document
+ let $$key := 'content'
+ let $$value := $$xDoc => cmark:dispatch()
+ return 
+   if ( $$value instance of item() ) 
+   then map:put($$map, $$key, $$value)
+   else map:put($$map,'problem','unable to parse content')
+};
+
+declare function _:getFrontMatter( $$map as map(*)) as map(*){
+if ( map:contains($$map, 'problem') )
+then ( $$map )
+else
+let $$xDoc :=  $$map?document
+let $$fm  := $$xDoc => cmark:frontmatter()
+return
+if ( $$fm instance of map(*) ) 
+then map:merge(($$map, $$fm ))
+else map:put( $$map, 'problem', 'unable to get frontmatter')
+};
+
+declare function _:pageLayout( $$map as map(*)) {
+if ( map:contains( $$map, 'problem') )
+then $$map  => _:svrErr()
+else 
+let $$dbItemPath := concat( $$map?collection ,'/',$$map?layout ) 
+let $$dbCollection :=  $$map?collection => uri-collection()
+let $$defaultLayout := concat($$map?collection,'/default_layout' )
+return
+if ( $$dbItemPath = $$dbCollection ) then
+  let $$dbFunctionItem := $$dbItemPath => db:get()
+  return 
+    if ( $$dbFunctionItem instance of function(*) ) 
+    then ( _:resHeader( map { 'status': '200', 'message': 'OK' } ),$$map => $$dbFunctionItem())
+    else _:svrErr( map:put($$map,'problem',``[
+ - xqerl database XDM item: `{$$dbItemPath}` is not a XDM function
+-  a XDM function item in needed to render this page
+ ]``))
+else (: try the default layout function for collection:)
+if ( $$defaultLayout = $$dbCollection ) then
+  let $$dbFunctionItem := $$defaultLayout  => db:get()
+  return 
+    if ( $$dbFunctionItem instance of function(*) ) 
+    then ( _:resHeader( map { 'status': '200', 'message': 'OK' } ),$$map => $$dbFunctionItem())
+    else 
+    _:svrErr( map:put($$map,'problem',``[
+ - xqerl database XDM item: `{$$defaultLayout}` is not a XDM function
+-  a XDM function item in needed to render this page
+ ]``))
+else (: give up :)
+_:svrErr( map:put($$map,'problem', 
+``[
+ - xqerl database does not contain XDM item: `{$$dbItemPath}`
+-  this XDM function item is need to render this page
+ ]``))
+};
+
+declare function _:dbCollection( $$uri ){
+let $$dbPathTokens := $$uri => tokenize('/')
+let $$dbPathTokensCount := $$dbPathTokens => count()
+return
+ $$dbPathTokens => remove($$dbPathTokensCount ) => string-join('/')
+};
+
+declare function _:dbItem( $$uri ){
+let $$dbPathTokens := $$uri => tokenize('/')
+let $$dbPathTokensCount := $$dbPathTokens => count()
+return
+$$dbPathTokens => subsequence($$dbPathTokensCount)
+};
+
+declare function _:notFound( $$map as map(*) ) as item()+ {(
+_:resHeader( map { 'status': '404', 'message': 'Not Found' }),
+_:problemBody( map:put($$map, 'problem', ``[ could not find document at `{ $$map?uri }` ]``))
+)};
+
+declare function _:svrErr( $$map as map(*)) as item()+ {(
+_:resHeader( map { 'status': '500', 'message': 'Internal Server Error' }),
+ _:problemBody( map:put($$map,'problem_title','server error: xqerl thrown error') )
+)};
+
+declare function _:problemBody( $$map as map(*)) {
+element html {
+  attribute lang {'en'},
+  element head {
+    element title { 'Huston We Have A Problem' }
+  },
+  element body {
+    element p { 'erewhon: we have a problem' },
+    element dl {(
+      if ( map:contains($$map, 'uri')) then (element dt { 'uri' }, element dd { $$map?uri}) else (),
+      if ( map:contains($$map, 'problem')) then (element dt { 'problem' }, element dd { $$map?problem}) else (),
+      if ( map:contains($$map, 'code')) then (element dt { 'code' }, element dd { $$map?code}) else (),
+      if ( map:contains($$map, 'description')) then (element dt { 'description' }, element dd { $$map?description}) else ()
+    )}
+  }
+}
+};
+endef
+
+
+define index_md
+ <!--
+ title: Alter Me 
+ layout: default_layout
+-->
+
+## A boilerplate document
+
+The source for this page is a markdown document located in `src/data/$(DEV_DOMAIN)/index.md`
+
+The source is **stored** in the xqerl database as a
+[cmark](https://github.com/commonmark/cmark) converted XML document.
+
+The frontmatter section is a HTML comment block at the 
+start of the document
+
+Page 'layout' can be specified in the frontmatter.
+If no page layout is specified then a default layout will be used.
+Layout refers to xQuery function item **stored** in the xqerl database.
+The layout source is `src/data/$(DEV_DOMAIN)/default_layout.xq`
+
+The xqerl database can store any XDM item. 
+
+endef
+
